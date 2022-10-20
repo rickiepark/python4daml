c235516537a6bae87facbbd7700fe0639e059220
Haesun Park <haesunrpark@gmail.com>
2022-10-20 18:32:59 +0900

    Create circle.py

---
+++ circle.py
@@ -0,0 +1,13 @@
+pi = 3.14159
+
+def area(radius):
+    return pi*(radius**2)
+
+def circumference(radius):
+    return 2*pi*radius
+
+def sphere_surface(radius):
+    return 4.0*area(radius)
+
+def sphere_volume(radius):
+    return (4.0/3.0)*pi*(radius**3)
